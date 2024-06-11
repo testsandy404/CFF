e393e7f8a305fa8003ba978be372732b5c20d74a
SandeshP09 <sandeshprabhu85@gmail.com>
2024-06-11 11:02:23 +0530

    changes in About Us

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -4,62 +4,68 @@
 @section('content')
 
 <!-- Single Page Header start -->
-<div class="container-fluid py-5">
+<div class="container-fluid py-4">
     <div class="text-center mx-auto" style="max-width: 700px;">
         <h1 class="display-4">About Us</h1>
     </div>
 </div>
 
 {{-- Message card --}}
-<div class="container pb-4">
-    <div class="card" style="max-width: auto; min-height: auto; margin-left: 150px; margin-right: 150px;">
-        <div class="row no-gutters">
+<div class="container p-0">  
+        <div class="row justify-content-center">
           <div class="col-md-4">
-            <img src="storage\uploads\about_us\topgun.jpg" class="card-img" alt="Image of a person" style="width:100%; height:100%;">
-          </div>
-          <div class="col-md-8 p-0">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <img src="storage\uploads\about_us\chefs.jpeg" class="card-img" alt="Image of a person" style="width:100%; height:85%;">
+            {{-- <div class="text-center pt-3" style="font-size:18px; color:black;">
+                <b>Promoter Chairman Chefs Frozen Foods Pvt. Ltd.</b> --}}
+                <div class="text-center pt-3">
+                    <b style="color:rgb(52, 81, 222); font-size:18px;"><em>"Quality is not just a standard, it’s an attitude."</em></b>
+                    <br>
+                    <b style="font-size:20px; color:black;">Pranay Thakker</b>
+                    <br>
+                    <b style="color:black;">(Promoter Chairman Chefs Frozen Foods Pvt. Ltd.)</b>
+                </div>
             </div>
-          </div>
-        </div>
+        </div>    
       </div>
 </div>
 
 
+
 <div class="container-fluid featurs">
-    <div class="container py-2">
+    <div class="container pt-0">
         <div class="row justify-content-center pb-3">
             <div class="d-flex align-items-center justify-content-center mt-4">
-                <h2 class="text-primary pb-1">Who we are</h2>
+                <h2 class="text-primary pb-1">Our Journey</h2>
             </div>
-            <div class="text-center mx-auto" style="width: 80%;">
-                <p>At Chef's Frozen Food, we are dedicated to revolutionizing the frozen food industry with our unwavering commitment to quality and innovation. Since our founding, we have built a reputation for excellence, driven by our passion for creating delicious and convenient meals. Our team of culinary experts and food technologists work tirelessly to set new benchmarks in quality and taste. Every product we create reflects our dedication to culinary excellence and customer satisfaction, ensuring that our offerings meet the highest standards of the food industry.</p>
+            <div class="text-center mx-auto" style="width: 80%; font-size:18px; color:black;">
+                <p>CFFPL was incorporated in 2006 with a vision to provide top-notch frozen fruits and vegetables to international markets. Over the years, our dedication to quality and customer satisfaction has enabled us to expand our horizons and enter the local market successfully. </p>
             </div>
         </div>
         <div class="row justify-content-center pb-3">
             <div class="d-flex align-items-center justify-content-center mt-4">
-                <h2 class="text-primary pb-1">What we offer</h2>
+                <h2 class="text-primary pb-1">Sales and Distribution Excellence</h2>
             </div>
-            <div class="text-center mx-auto" style="width: 80%;">
-                <p>Chef's Frozen Food presents a comprehensive range of frozen food products, tailored to meet the diverse needs of our customers. Our portfolio includes gourmet dishes, indulgent desserts, health-focused meals, and quick snacks, all crafted with premium ingredients. We are committed to providing products that offer exceptional flavor, nutrition, and convenience, making us a trusted choice for discerning consumers. Our products are designed to simplify meal preparation while maintaining high standards of taste and nutrition, allowing you to enjoy quality meals effortlessly.</p>
+            <div class="text-center mx-auto" style="width: 80%; font-size:18px; color:black;">
+                <p>It has been an honor to deliver exceptional sales and distribution services across various sales channels, empowering brands to broaden their market reach and achieve new heights of success. We are relentless in our pursuit to push the boundaries, constantly developing innovative solutions for both brands and customers.</p>
             </div>
         </div>
         <div class="row justify-content-center pb-3">
             <div class="d-flex align-items-center justify-content-center mt-4">
                 <h2 class="text-primary pb-1">Our Vision</h2>
             </div>
-            <div class="text-center mx-auto" style="width: 80%;">
-                <p>Our vision at Chef's Frozen Food is to become a global leader in the frozen food sector, known for our unwavering quality and culinary excellence. We aspire to change the way consumers perceive and enjoy frozen food by consistently offering innovative and delicious products. Our goal is to set new industry standards, ensuring that our customers always receive the best in taste, nutrition, and convenience. Through continuous improvement and a commitment to excellence, we aim to inspire confidence and satisfaction in every bite, making Chef's Frozen Food a household name worldwide.</p>
+            <div class="text-center mx-auto" style="width: 80%; font-size:18px; color:black;">
+                <p>At CFFPL, our vision is to be a leading provider of high-quality food products, trusted by customers and our channel partners. We strive to achieve this through continuous innovation, commitment to quality, and a customer-centric approach. Our goal is not just to meet the expectations of our customers but to exceed them, ensuring that every product we deliver is a testament to our dedication and excellence.</p>
             </div>
         </div>
+        
+        
         <div class="row justify-content-center pb-3">
             <div class="d-flex align-items-center justify-content-center mt-4">
-                <h2 class="text-primary pb-1">Our Mission</h2>
+                <h2 class="text-primary pb-1">Looking Ahead</h2>
             </div>
-            <div class="text-center mx-auto" style="width: 80%;">
-                <p>Chef's Frozen Food's mission is to elevate the frozen food experience by delivering products that combine gourmet quality, nutritional value, and convenience. We are dedicated to making exceptional frozen foods accessible to everyone, regardless of their lifestyle. By sourcing our ingredients responsibly and committing to sustainable practices, we aim to minimize our environmental impact while ensuring the highest standards of quality and taste. Through our relentless pursuit of innovation and excellence, we seek to build lasting trust and satisfaction with our customers, fostering a loyal and delighted customer base.</p>
+            <div class="text-center mx-auto" style="width: 80%; font-size:18px; color:black;">
+                <p>As we continue to grow, our commitment to quality and innovation remains unwavering. We are excited about the future and the opportunities it holds for us and our partners. Together, we can achieve greater heights and set new standards in the foods industry.
+                    Thank you for being a part of our journey. We look forward to serving you with the best frozen foods, backed by our promise of quality and excellence.</p>
             </div>
         </div>
     </div>
